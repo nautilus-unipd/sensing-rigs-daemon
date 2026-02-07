7fda1ad4f3a24e0ab7bbfd43a718cf173effca16
paznico <niccolo.pasetto@studenti.unipd.it>
2026-02-07 19:25:23 +0100

    Reduced time to needed to take a picture

--- sensing_rigs_daemon/src/constants.c
+++ sensing_rigs_daemon/src/constants.c
@@ -2,7 +2,7 @@
 
 const char* restrict ROOT_PATH = "/";
 const char* restrict REDIRECT_FILE = "/dev/null";
-const char* restrict DAEMON_PATH = "/demon/run/";
+const char* restrict DAEMON_PATH = "/home/berry/sensing-rigs-daemon/run/";
 const char* restrict DAEMON_PATH_LOG = "sensing_rigs_daemon.log.d/";
 const char* restrict DAEMON_PATH_PID = "sensing_rigs_daemon.pid";
 const char* restrict DAEMON_PATH_CAP = "captures/";
@@ -12,7 +12,7 @@ const char* restrict BASE_LOG_FILE = "%04d-%02d-%02d.log";
 const char* restrict BASE_LOG_ENTRY = "%02d:%02d:%02d, %c, %s\n";
 const char* restrict FILE_PATH_TEMP = "/sys/devices/virtual/thermal/thermal_zone0/temp";
 const char* restrict VOLT_COMMAND = "vcgencmd get_throttled | sed -e 's/^throttled=0x//'";
-const char* restrict PHOTO_SHOOT_COMMAND = "rpicam-still -v 0 --zsl --width 1920 --height 1080 --camera %d -o %s%s%s%.2d%.2d%.2d.jpg";
+const char* restrict PHOTO_SHOOT_COMMAND = "rpicam-still -v 0 --zsl --width 1920 --height 1080 --camera %d --timestamp --immediate -o %s%s%s";
 
 const char VOLT_CORRECT_1 = 0x30;
 const char VOLT_CORRECT_2 = 0x0a;
@@ -23,4 +23,4 @@ const uint8_t MAX_MSG_SIZE = 50;
 const unsigned long int MIN_FREE_SPACE = 590000;
 const uint32_t MAX_TEMP = 65000;
 const uint8_t MAX_VOLT_BUFF_SIZE = 19;
-const uint8_t MAX_COMMAND_SIZE = 125;
+const uint8_t MAX_COMMAND_SIZE = 140;
